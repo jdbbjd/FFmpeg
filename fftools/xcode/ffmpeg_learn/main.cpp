faa8037c80ce17a6c49924a263d861e752b11751
jdbbjd <1090336995@qq.com>
2022-11-09 19:51:10 +0800

    Create workspace and create FFmpegLearn

---
+++ fftools/xcode/ffmpeg_learn/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ffmpeg_learn
+//
+//  Created by T-MBP on 2022/11/9.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
